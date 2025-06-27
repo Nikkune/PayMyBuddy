554251c86338b1ef84fa0c69c26bd0749c39597d
Nikkune <dev.2tvhg@passmail.net>
2025-06-27 17:57:07 +0200

    refactor: 🔨 Update schema generation strategy to use SEQUENCE
    - Changed `@GeneratedValue` strategy for `User` and `Transaction` entities from `IDENTITY` to `SEQUENCE` for better sequence-based ID management.
    - Modified `application.properties` to update schema generation behavior (`drop-and-create` strategy).
    - Added new sequence tables (`users_seq` and `transactions_seq`) in `generated-schema.sql` to accommodate the SEQUENCE strategy.
    - Refined schema scripts to ensure consistency and proper sequence handling for primary key generation.

--- src/main/java/dev/nikkune/paymybuddy/model/Transaction.java
+++ src/main/java/dev/nikkune/paymybuddy/model/Transaction.java
@@ -8,7 +8,7 @@ import lombok.Data;
 @Data
 public class Transaction {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int id;
 
     @ManyToOne
--- src/main/java/dev/nikkune/paymybuddy/model/User.java
+++ src/main/java/dev/nikkune/paymybuddy/model/User.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Data
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int id;
 
     @Column(unique = true)
